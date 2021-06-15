ff6e1129684e6aab1c32501e7a2fc9b67d22e64b
Avi Khandakar <avi@Avis-MacBook-Air.local>
2021-06-15 08:19:47 +0600

    visual changes

--- components/Footer.js
+++ components/Footer.js
@@ -126,7 +126,7 @@ const Footer = () => {
           </a>
         </div>
         <p className="mt-8 text-base leading-6 text-center text-gray-400">
-          © 2021 BUET 87, Inc. All rights reserved.
+          © 2021 BUETIAN 89, Inc. All rights reserved.
         </p>
       </div>
     </section>
