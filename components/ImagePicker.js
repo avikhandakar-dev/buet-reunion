df58c2aed0e3996827862c175e57c3ad0c82fa56
Avi Khandakar <avi@Avis-MacBook-Air.local>
2021-07-05 09:12:58 +0600

    fix image size

--- components/ImagePicker.js
+++ components/ImagePicker.js
@@ -86,56 +86,66 @@ const ImagePicker = ({
                     <IoClose />
                   </a>
                 </Dialog.Title>
-                <div className="mt-2 px-6">
-                  {media.length < 1 ? (
-                    <Fragment>
-                      <div className="py-4 px-5 bg-gray-50 dark:bg-gray-700 h-96 flex flex-col justify-center items-center">
-                        <Empty
-                          width={150}
-                          className="text-gray-600 dark:text-gray-200"
-                        />
-                        <div className="mt-3">
-                          <Link href="/admin/media/upload">
-                            <a className="flex border-2 rounded px-5 py-3 dark:border-gradient-1-start border-gradient-4-start dark:text-gradient-1-start text-gradient-4-start uppercase font-medium justify-center items-center transition-colors duration-300 hover:text-yellow-800 dark:hover:bg-gradient-1-start hover:bg-gradient-4-start">
-                              <FaCloudUploadAlt className="mr-2 text-xl" />
-                              Upload some images
-                            </a>
-                          </Link>
+                <div
+                  style={{ maxHeight: "80vh" }}
+                  className="flex flex-col justify-between"
+                >
+                  <div className="px-6 overflow-y-auto">
+                    {media.length < 1 ? (
+                      <Fragment>
+                        <div className="py-4 px-5 bg-gray-50 dark:bg-gray-700 h-96 flex flex-col justify-center items-center">
+                          <Empty
+                            width={150}
+                            className="text-gray-600 dark:text-gray-200"
+                          />
+                          <div className="mt-3">
+                            <Link href="/admin/media/upload">
+                              <a className="flex border-2 rounded px-5 py-3 dark:border-gradient-1-start border-gradient-4-start dark:text-gradient-1-start text-gradient-4-start uppercase font-medium justify-center items-center transition-colors duration-300 hover:text-yellow-800 dark:hover:bg-gradient-1-start hover:bg-gradient-4-start">
+                                <FaCloudUploadAlt className="mr-2 text-xl" />
+                                Upload some images
+                              </a>
+                            </Link>
+                          </div>
+                        </div>
+                      </Fragment>
+                    ) : (
+                      <div className="py-4 bg-gray-50 dark:bg-gray-700">
+                        <div className="grid grid-cols-3 sm:grid-cols-4 xl:grid-cols-6 justify-around justify-items-stretch gap-4 relative">
+                          {media.map((image) => (
+                            <Fragment>
+                              <div
+                                key={image.id}
+                                className="overflow-hidden relative group rounded-md"
+                              >
+                                <div className="w-full relative overflow-hidden rounded-md h-auto transform group-hover:scale-110 transition duration-700">
+                                  <Image
+                                    src={image.downloadUrl}
+                                    width={150}
+                                    height={150}
+                                    priority={true}
+                                    objectFit="cover"
+                                    layout="responsive"
+                                    sizes="(max-width: 640px) 100px, (max-width: 1024px) 150px, 150px"
+                                    loading="eager"
+                                  />
+                                </div>
+                              </div>
+                            </Fragment>
+                          ))}
                         </div>
                       </div>
-                    </Fragment>
-                  ) : (
-                    <div className="py-4 px-5 bg-gray-50 dark:bg-gray-700">
-                      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 justify-around justify-items-stretch gap-4 relative">
-                        {media.map((image) => (
-                          <Fragment>
-                            <div className="overflow-hidden relative group rounded-md">
-                              <div className="w-full relative overflow-hidden rounded-md h-auto transform group-hover:scale-110 transition duration-700">
-                                <Image
-                                  src={image.downloadUrl}
-                                  width={150}
-                                  height={150}
-                                  priority={true}
-                                  objectFit="cover"
-                                  layout="responsive"
-                                />
-                              </div>
-                            </div>
-                          </Fragment>
-                        ))}
-                      </div>
-                    </div>
-                  )}
-                </div>
+                    )}
+                  </div>
 
-                <div className="mt-4 px-6 py-3">
-                  <button
-                    type="button"
-                    className="inline-flex justify-center px-4 py-2 text-sm font-medium text-green-900 bg-green-100 border border-transparent rounded-md hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-green-500"
-                    onClick={closeModal}
-                  >
-                    Got it, thanks!
-                  </button>
+                  <div className="px-6 py-3 flex-shrink-0 bg-gray-800 flex justify-end">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center px-16 py-2 text-sm font-medium text-white bg-primary border border-transparent rounded-md hover:bg-sky focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-green-500"
+                      onClick={closeModal}
+                    >
+                      Open
+                    </button>
+                  </div>
                 </div>
               </div>
             </Transition.Child>
