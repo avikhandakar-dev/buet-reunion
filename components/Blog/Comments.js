b1e540466288b3388e3765d0cb055c3724c086d6
Avi Khandakar <avi@Avis-MacBook-Air.local>
2021-10-30 11:54:40 +0600

    comment updated

--- components/Blog/Comments.js
+++ components/Blog/Comments.js
@@ -1,14 +1,34 @@
 import CommentsForm from "./CommentsForm";
+import ListComments from "./ListComments";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { firestore } from "@lib/firebase";
+import { CgSpinner } from "react-icons/cg";
 
 const Comments = ({ post }) => {
+  const [comments = [], loading, error] = useCollectionData(
+    firestore
+      .collection("comments")
+      .where("postInfo.id", "==", post.id)
+      .orderBy("createdAt", "desc")
+  );
   return (
     <div>
       <div className="border-b-2 border-gray-200 dark:border-gray-700 pb-2 mx-4">
         <p className="font-semibold text-gray-600 dark:text-gray-300">
-          Comments (0)
+          Comments ({comments.length})
         </p>
       </div>
       <CommentsForm post={post} />
+      {loading && (
+        <>
+          <div className="max-w-7xl mx-auto flex justify-center items-center mb-32">
+            <span className="inline-flex text-5xl animate-spin text-primary">
+              <CgSpinner />
+            </span>
+          </div>
+        </>
+      )}
+      {!loading && comments.length && <ListComments comments={comments} />}
     </div>
   );
 };
---
+++ components/Blog/ListComments.js
@@ -0,0 +1,30 @@
+import { serverTimestampToString, timestampToString } from "@lib/healper";
+import Image from "next/image";
+
+const ListComments = ({ comments }) => {
+  return (
+    <div className="mt-4 mx-4 mb-16 flex space-y-4 flex-col">
+      {comments.map((comment) => (
+        <div className="flex space-x-4">
+          <div className="border-2 flex-shrink-0 outline-none focus:outline-none border-gray-200 dark:border-gray-600 relative rounded-full overflow-hidden w-11 h-11">
+            <Image
+              src={comment.authorInfo.avatar || "/img/avatar.svg"}
+              width={44}
+              height={44}
+              objectFit="cover"
+            />
+          </div>
+          <div className="bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-700 rounded-lg px-4 py-2 sm:px-6 sm:py-4 leading-relaxed flex-1">
+            <strong>{comment.authorInfo.name}</strong>{" "}
+            <span class="text-xs text-gray-400">
+              {serverTimestampToString(comment.createdAt)}
+            </span>
+            <p>{comment.text}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ListComments;
